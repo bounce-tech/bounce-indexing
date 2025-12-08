069a5a4e7289c00a034039709137f6782266c804
Chase Manning <chase@manning.dev>
2025-12-08 16:56:43 +0000

    :sparkles: create get leveraged token data function

---
+++ src/api/utils/leveraged-token-data.ts
@@ -0,0 +1,45 @@
+import { Address } from "viem";
+import { publicClients } from "ponder:api";
+import { LT_HELPER_ADDRESS } from "../../../ponder.config";
+import { LeveragedTokenHelperAbi } from "../../../abis/LeveragedTokenHelperAbi";
+
+export interface LeveragedTokenData {
+  leveragedToken: Address;
+  marketId: number;
+  targetAsset: string;
+  targetLeverage: bigint;
+  isLong: boolean;
+  exchangeRate: bigint;
+  baseAssetBalance: bigint;
+  totalAssets: bigint;
+  userCredit: bigint;
+  credit: bigint;
+  agents: Address[];
+  balanceOf: bigint;
+  mintPaused: boolean;
+}
+
+const getLeveragedTokenData = async (
+  user?: Address
+): Promise<LeveragedTokenData[]> => {
+  // Querying leveraged token contracts
+  let leveragedTokenData: LeveragedTokenData[] = [];
+  if (user) {
+    leveragedTokenData = (await publicClients["hyperEvm"].readContract({
+      abi: LeveragedTokenHelperAbi,
+      address: LT_HELPER_ADDRESS,
+      functionName: "getLeveragedTokens",
+      args: [user, false],
+    })) as LeveragedTokenData[];
+  } else {
+    leveragedTokenData = (await publicClients["hyperEvm"].readContract({
+      abi: LeveragedTokenHelperAbi,
+      address: LT_HELPER_ADDRESS,
+      functionName: "getLeveragedTokens",
+      args: [],
+    })) as LeveragedTokenData[];
+  }
+  return leveragedTokenData;
+};
+
+export default getLeveragedTokenData;
